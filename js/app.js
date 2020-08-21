697064b800022c0f55de8b474cff043f91b18e64
SieChan <sie.chan@outlook.com>
2020-08-20 20:59:24 -0600

    Edited tweets and print to console loop

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var userTweets = ["a concept: Wear a mask that covers your mouth AND your nose", "its not a coincidence that the word diet has the word die in it", "eat, sleep, workout, repeat", "BLM", "Speak up for those who cannot", "healthy mind, body and soul is the focus to be a better you", "Tell people when you think about them, show up in the ways that you can before its too late", "The hardest part is actually going and showing up so just get through that", "You are looking at a future full stack web developer!", "Set healthy boundaries for others and for yourself."];
+console.log(userTweets);
+
+for (var counter = 0; counter < userTweets.length; counter++) {
+    console.log(userTweets[counter]);
+}
